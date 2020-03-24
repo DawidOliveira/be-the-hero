49ddcf85c59f7482e10aea8a718dfb55b9a6b068
Dawid <dawid.oliveira@arapiraca.ufal.br>
2020-03-24 14:21:00 -0300

    Backend da aplicação

---
+++ backend/src/controllers/IncidentController.js
@@ -0,0 +1,45 @@
+const connection = require('../database/connection');
+
+module.exports = {
+    async store(req, res) {
+        const {title, description, value} = req.body;
+        const ong_id = req.headers.authorization;
+
+        const data = {title, description, value, ong_id};
+
+        const [id] = await connection('incidents').insert(data);
+
+        return res.json({id});
+    },
+
+    async index(req, res){
+        const {page = 1} = req.query;
+
+        const [count] = await connection('incidents').count();
+
+        console.log(count);
+        const data = await connection('incidents').join('ongs', 'ongs.id','=','incidents.ong_id').limit(5).offset((page-1)*5).select(['incidents.*','ongs.name','ongs.whatsapp','ongs.city','ongs.uf']);
+
+        res.header('X-Total-Count',count['count(*)']);
+
+        return res.json(data);
+
+    },
+
+    async delete(req, res){
+        const {id} = req.params;
+        const ong_id = req.headers.authorization;
+
+        const incident = await connection('incidents').where('id', id).select('ong_id').first();
+        
+        if (ong_id !== incident.ong_id){
+            return res.status(401).json({
+                error: 'Operation not permitted.'
+            })
+        }
+
+        await connection('incidents').where('id',id).delete();
+
+        return res.status(204).send();
+    }
+}
