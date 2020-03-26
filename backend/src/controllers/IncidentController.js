251b587e8b395f601b9451332de44a12db687b94
Dawid <dawid.oliveira@arapiraca.ufal.br>
2020-03-26 16:59:38 -0300

    mobile pronto

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -18,7 +18,7 @@ module.exports = {
         const [count] = await connection('incidents').count();
 
         console.log(count);
-        const data = await connection('incidents').join('ongs', 'ongs.id','=','incidents.ong_id').limit(5).offset((page-1)*5).select(['incidents.*','ongs.name','ongs.whatsapp','ongs.city','ongs.uf']);
+        const data = await connection('incidents').join('ongs', 'ongs.id','=','incidents.ong_id').limit(5).offset((page-1)*5).select(['incidents.*','ongs.name','ongs.email','ongs.whatsapp','ongs.city','ongs.uf']);
 
         res.header('X-Total-Count',count['count(*)']);
 
